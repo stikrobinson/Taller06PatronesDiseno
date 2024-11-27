eb63526016a4993aad5a5f01d465f84eb66cc29a
Steve Robinson <srobinso@espol.edu.ec>
2024-11-27 10:06:55 -0500

    creacion del proyecto

---
+++ Taller06PatronesDiseno/src/taller06patronesdiseno/Taller06PatronesDiseno.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller06patronesdiseno;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller06PatronesDiseno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
